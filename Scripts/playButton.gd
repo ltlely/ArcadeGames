b8847ea7c0437b257eb3682f9b5c06435a4d9561
Lynnithe <amanda.dang1202@gmail.com>
2024-09-08 14:32:21 -0400

    updated file path for play button

--- Scripts/playButton.gd
+++ Scripts/playButton.gd
@@ -1,4 +1,4 @@
 extends Button
 
 func _on_pressed() -> void:
-	get_tree().change_scene_to_file("res://ArcadeGames/Scenes/firstWorld.tscn")
+	get_tree().change_scene_to_file("res://Scenes/firstWorld.tscn")
