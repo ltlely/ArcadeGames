79534b469a448fe6e04aa2eb539fe63560636344
ltlely <lylytr.311@gmail.com>
2024-10-09 10:11:11 -0400

    changed connect pressed

--- Scenes/help_button.gd
+++ Scenes/help_button.gd
@@ -1,13 +1,13 @@
 extends Control  # Make sure this is attached to the root node or scene.
 
 # Declare the button and popup
-onready var button = $HelpButton
-onready var popup_panel = $Popup
+@onready var button = $HelpButton
+@onready var popup_panel = $Popup
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Connect the button pressed signal to a function
-	button.connect("pressed", self, "_on_button_pressed")
+	button.pressed.connect(_on_button_pressed)
 
 # Function that is called when the button is pressed
 func _on_button_pressed():
